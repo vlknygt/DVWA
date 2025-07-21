8071f7ef76bea98fd38fc25dc7b48575da7a21d7
Volkan Yiğit <91802138+vlknygt@users.noreply.github.com>
2025-07-21 16:41:26 +0300

    Update low.php

--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -6,7 +6,7 @@ header ("X-XSS-Protection: 0");
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {//test2222
 	// Feedback for end user
 	// Vulnerable?
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
+	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';//test333333
 }
 
 ?>
